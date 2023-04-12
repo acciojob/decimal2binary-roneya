6147aa33b946c428d97c169d59b78be160db6622
Rohan Vidhate <49265832+roneya@users.noreply.github.com>
2023-04-12 12:09:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+const decimalToBinary = (num) => {
+  let binaryStr = num.toString(2);
+  return binaryStr;
+};
 
-function decimalToBinary(num){
-  
-let binaryStr = num.toString(2);
-return binaryStr;
-}
+const num = prompt("Enter a decimal number:");
+const binaryStr = decimalToBinary(parseInt(num));
+alert(binaryStr);
 
 module.exports = decimalToBinary;
