e3ae75cbdd5e1c352919cd0bb3d29375413c43a8
Rohan Vidhate <49265832+roneya@users.noreply.github.com>
2023-04-12 11:38:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
 
 function decimalToBinary(num){
-    let binaryStr = ""
-    while(num>0){
-
-        let rem = num%2;
-
-        binaryStr = binaryStr + rem.toString()
-
-        num = Math.floor(num/2)
-    }
-
-    return binaryStr.split("").reverse().join("")
+  
+let binaryStr = num.toString(2);
+console.log(binaryStr);
 }
 
 module.exports = decimalToBinary;
